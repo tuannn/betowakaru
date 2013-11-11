51b3259344e52185fcb1dca268813dc599010483
dinhthanhtam <phuonglh0420@gmail.com>
2013-11-11 14:19:56 +0700

    Template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Betowakaru::Application.config.secret_key_base = 'eb42c0840c54472f8247efb405e3cec33ff80f17eefb5b73d2abf212301d96f5c7219813349ff132eeba54549319b1f5a5ddd67c08d426e80778f0b7534977db'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Betowakaru::Application.config.session_store :cookie_store, key: '_betowakaru_session'
